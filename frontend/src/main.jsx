190b29c6f15bb7f738898dde95d869f40b5bc126
AMIT YADAV <91722003+DevAmitYadav@users.noreply.github.com>
2025-03-17 08:23:29 +0530

    Update main.jsx
    Included HashRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom"; // Import HashRouter
 import store from "./redux/store"; // Import Redux store
 import "./index.css";
 import App from "./App.jsx";
@@ -8,7 +9,9 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}> {/* Wrap your app in Provider */}
-      <App />
+      <HashRouter> {/* Use HashRouter to prevent 404 issues */}
+        <App />
+      </HashRouter>
     </Provider>
   </StrictMode>
 );
