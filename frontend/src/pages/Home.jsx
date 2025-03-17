779e48cba21351a5e2e189bbd53b12b2714f5b1b
AMIT YADAV <91722003+DevAmitYadav@users.noreply.github.com>
2025-03-17 08:24:04 +0530

    Update Home.jsx
    Navigation Fix

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,9 @@
 // src/pages/Home.jsx
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+    const navigate = useNavigate();
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-800 via-gray-700 to-gray-900 p-4">
       <div className="bg-white bg-opacity-90 backdrop-filter backdrop-blur-md rounded-lg shadow-xl p-10 max-w-xl text-center">
@@ -13,11 +15,11 @@ const Home = () => {
           designed for modern category management.
         </p>
         <button
-          onClick={() => (window.location.href = "/register")}
-          className="px-6 py-3 bg-gradient-to-r from-indigo-800 to-pink-600 text-white rounded-full font-semibold hover:opacity-90 transition duration-200"
-        >
-          Get Started
-        </button>
+      onClick={() => navigate("/register")}
+      className="px-6 py-3 bg-gradient-to-r from-indigo-800 to-pink-600 text-white rounded-full font-semibold hover:opacity-90 transition duration-200"
+    >
+      Get Started
+    </button>
       </div>
     </div>
   );
